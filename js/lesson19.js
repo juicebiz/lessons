54a11aff9afcaa2386b5b2d137066040039694a0
Ivan Klyushin <juice_biz@mail.ru>
2020-11-30 23:07:27 +0300

    lesson 19

---
+++ js/lesson19.js
@@ -0,0 +1,27 @@
+"use strict";
+
+function first() {
+    setTimeout(function(){
+        console.log(1);
+    }, 500);
+}
+
+function second() {
+    console.log(2);
+}
+
+first();
+second();
+
+function learnJS(lang, callback) {
+    console.log(`Я учу: ${lang}`);
+    callback();
+}
+
+
+
+function done() {
+    console.log('Я прошёл другой урок');
+}
+
+learnJS('JavaScript', done);
