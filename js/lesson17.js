386edc1af52531b636628a9dfbe7b5a841c70eee
Ivan Klyushin <juice_biz@mail.ru>
2020-11-30 21:46:23 +0300

    lesson 17

---
+++ js/lesson17.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const str = "Test";
+const arr = [1, 2, 3];
+
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+
+console.log(str);
+
+let fruit = "Some fruit";
+
+console.log(fruit.indexOf("q"));
+
+const log = "Hello World";
+console.log(log.slice(6));
+console.log(log.slice(-5, 0));
+
+console.log(log.substring(6, 11));
+
+console.log(log.substr(6, 5));
+
+const num = 12.2;
+console.log(Math.round(num));
+
+const test = "12.2px";
+console.log(parseInt(test));
+console.log(parseFloat(test));
